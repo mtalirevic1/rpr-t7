fbd2f21618b68fd0928dff200180ce3420a85f66
Matej <mtalirevic1@etf.unsa.ba>
2018-12-05 13:29:27 +0100

    Initial commit

---
+++ src/rpr/Grad.java
@@ -0,0 +1,39 @@
+package rpr;
+
+import java.io.Serializable;
+
+public class Grad implements Serializable {
+
+    private String naziv;
+    private int brojStanovnika;
+    private double [] temperature=new double [1000];
+
+    Grad(String naziv, int brojStanovnika){
+        this.naziv=naziv;
+        this.brojStanovnika=brojStanovnika;
+    }
+
+    public double[] getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double[] temperature) {
+        this.temperature = temperature;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public int getBrojStanovnika() {
+        return brojStanovnika;
+    }
+
+    public void setBrojStanovnika(int brojStanovnika) {
+        this.brojStanovnika = brojStanovnika;
+    }
+}
