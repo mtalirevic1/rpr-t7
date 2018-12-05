142adc7244724ba5425e0214b0ec9cdba19c8f60
mtalirevic1 <mtalirevic1@etf.unsa.ba>
2018-12-06 00:17:05 +0100

    Metoda zapisiXml

--- src/rpr/Tutorijal.java
+++ src/rpr/Tutorijal.java
@@ -1,8 +1,10 @@
 package rpr;
 
 import java.beans.XMLDecoder;
+import java.beans.XMLEncoder;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -11,10 +13,11 @@ public class Tutorijal {
 
     public static ArrayList<Grad> ucitajGradove() {
         Scanner ulaz;
-        ArrayList<Grad> gradovi=new ArrayList<>();
+        ArrayList<Grad> gradovi = new ArrayList<>();
         try {
             ulaz = new Scanner(new FileReader("mjerenja.txt")).useDelimiter("[\\r,]");
-        } catch (FileNotFoundException e) {
+        }
+        catch (FileNotFoundException e) {
             System.out.println("Datoteka mjerenja.txt ne postoji ili se ne moze otvoriti.");
             System.out.println("Greska: " + e);
             return gradovi;
@@ -23,43 +26,46 @@ public class Tutorijal {
         try {
             while (ulaz.hasNext()) {
                 Grad grad = new Grad();
-                double [] niz=new double[1000];
+                double[] niz = new double[1000];
                 String naziv = ulaz.next();
-                int i=0;
+                int i = 0;
                 while (ulaz.hasNextDouble()) {
                     niz[i] = Double.parseDouble(ulaz.next());
                     i++;
-                    if(i==1000) break;
+                    if (i == 1000) break;
                 }
-                String naziv2=naziv;
+                String naziv2 = naziv;
                 grad.setNaziv(naziv);
                 grad.setTemperature(niz);
                 gradovi.add(grad);
             }
 
-        } catch (Exception e) {
+        }
+        catch (Exception e) {
             System.out.println("Problem pri citanju podataka.");
             System.out.println("Greska: " + e);
-        } finally {
+        }
+        finally {
             ulaz.close();
         }
         return gradovi;
     }
 
-    public static UN ucitajXml(ArrayList<Grad> gradovi){
-        UN unitedNations= null;
+    public static UN ucitajXml(ArrayList<Grad> gradovi) {
+        UN unitedNations = null;
 
-        try{
-            XMLDecoder ulaz= new XMLDecoder(new FileInputStream("drzave.xml"));
-            unitedNations=(UN) ulaz.readObject();
+        try {
+            XMLDecoder ulaz = new XMLDecoder(new FileInputStream("drzave.xml"));
+            unitedNations = (UN) ulaz.readObject();
             ulaz.close();
-        }catch(Exception e){
-            System.out.println("Greska: "+e);
+        }
+        catch (Exception e) {
+            System.out.println("Greska: " + e);
         }
 
-        for(Drzava d: unitedNations.getDrzave()){
-            for(int i=0; i<gradovi.size();i++){
-                if(d.getGlavniGrad().getNaziv().equals(gradovi.get(i).getNaziv())){
+        for (Drzava d : unitedNations.getDrzave()) {
+            for (int i = 0; i < gradovi.size(); i++) {
+                if (d.getGlavniGrad().getNaziv().equals(gradovi.get(i).getNaziv())) {
                     d.getGlavniGrad().setTemperature(gradovi.get(i).getTemperature());
                 }
             }
@@ -68,17 +74,29 @@ public class Tutorijal {
         return unitedNations;
     }
 
+    public static void zapisiXml(UN unitedNations) {
+
+        try {
+            XMLEncoder izlaz = new XMLEncoder(new FileOutputStream("un.xml"));
+            izlaz.writeObject(unitedNations);
+            izlaz.close();
+        }
+        catch (Exception e) {
+            System.out.println("Greska: " + e);
+        }
+    }
+
     public static void main(String[] args) {
-        ArrayList<Grad> gradovi=new ArrayList<>(ucitajGradove());
-        for(Grad g: gradovi){
-            System.out.print(g.getNaziv()+" ");
-            for(double x: g.getTemperature()){
-                System.out.print(x+" ");
+        ArrayList<Grad> gradovi = new ArrayList<>(ucitajGradove());
+        for (Grad g : gradovi) {
+            System.out.print(g.getNaziv() + " ");
+            for (double x : g.getTemperature()) {
+                System.out.print(x + " ");
             }
         }
 
 
-        for(Grad g: gradovi){
+        for (Grad g : gradovi) {
             System.out.print(g.getNaziv());
             //System.out.println();
         }
